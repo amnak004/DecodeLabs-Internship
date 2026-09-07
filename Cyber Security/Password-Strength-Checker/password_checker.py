d066f662f581972a695e15f9647d93489a51e35c
amnak004 <amnaahmed0029@gmail.com>
2026-09-07 01:49:50 -0700

    Add Password Strength Checker

---
+++ Cyber Security/Password-Strength-Checker/password_checker.py
@@ -0,0 +1,14 @@
+password = input("Enter your password: ")
+length = len(password)
+has_uppercase = any(char.isupper() for char in password)
+has_number = any(char.isdigit() for char in password)
+has_symbol = any(not char.isalnum() for char in password)
+
+if length < 8:
+    strength = "Weak"
+elif has_uppercase and has_number and has_symbol:
+    strength = "Strong"
+else:
+    strength = "Medium"
+
+print("Password strength: ", strength)
